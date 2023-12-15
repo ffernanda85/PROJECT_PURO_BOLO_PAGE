8745a6012dcd7a11cf3d203f0023d869fd61aa6b
ffernanda85 <f.fernanda85@gmail.com>
2023-12-15 20:29:31 -0300

    feat: adjusted footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,20 @@
-import * as s from "./styledFooter"
+import * as s from "./styledFooter";
 
 export const Footer = () => {
-    return (
-        <footer>
-            <h1>Footer</h1>
-        </footer>
-    )
-}
+  return (
+    <s.FooterContainer>
+      <s.Content>
+        <s.LogoFooter
+          src="/static/images/LOGO/logo.jpeg"
+          alt="Logo Puro Bolo"
+        />
+      </s.Content>
+
+      <s.WhatsappContainer>
+        <a href="https://web.whatsapp.com/" target="_blank">
+          <s.WhatsappButton src="/static/images/whatsapp.png" alt="Whatsapp" />
+        </a>
+      </s.WhatsappContainer>
+    </s.FooterContainer>
+  );
+};
--- src/components/footer/styledFooter.jsx
+++ src/components/footer/styledFooter.jsx
@@ -1,1 +1,24 @@
 import styled from 'styled-components'
+
+export const FooterContainer = styled.footer`
+    background-color:  #ddb2ba;
+    height: 30vh;
+`
+export const Content = styled.div`
+    width: 90vw;
+    height: 100%;
+    margin: 0 auto;
+    padding: 15px 0;
+`
+export const LogoFooter = styled.img`
+    width: 100px;
+    mix-blend-mode: multiply;
+`
+export const WhatsappContainer = styled.div`
+    position: fixed;
+    bottom: 32vh;
+    right: 5vh;
+`
+export const WhatsappButton = styled.img`
+    width: 40px;
+`
